4126c6a2979d6f8284829add09f3debd0752625f
M-Polina <p.marusina.2.9@gmail.com>
2023-11-24 22:02:15 +0300

    add loading time

---
+++ js/load_time.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    let pageloadtime = performance.timing.loadEventStart - performance.timing.navigationStart;
+    const pageEnd = performance.mark('pageEnd');
+    const loadTime = pageEnd.startTime;
+
+    var div_page_load_time = document.getElementsByClassName("page_load_time")[0];
+    div_page_load_time.textContent = `Page load time is ${pageloadtime / 1000} (${loadTime / 1000}) seconds`;
+});
